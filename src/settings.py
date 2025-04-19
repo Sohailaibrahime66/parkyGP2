2aa1427942bc99ff6c9521fd8533b95966c21e6a
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 20:26:04 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -155,7 +155,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smartization_parky',
+        'NAME': 'smartization_parkings',
         'USER': '405565',
         'PASSWORD': '123456@smart',
         'HOST': 'mysql-smartization.alwaysdata.net',
