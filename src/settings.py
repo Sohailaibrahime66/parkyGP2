c25854a37aed7718bbac9013785eeeac50467509
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-22 00:46:38 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -46,7 +46,11 @@ INSTALLED_APPS = [
     'corsheaders',
     'parking',
 ]
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 
 # REST_FRAMEWORK = {
